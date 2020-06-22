51d7b0bc19f207b8b6eb665189aa661579965dff
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-06-22 19:55:41 -0300

    Projeto Criado

---
+++ src/test/java/com/tarcisiolopes/moneyapi/FinancasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarcisiolopes.moneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
