027323315acf2a190b704a568d2bceb33aec916d
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-06-22 20:08:31 -0300

    Categoria - Modelo e Recurso

---
+++ src/main/java/com/tarcisiolopes/moneyapi/resources/CategoriaResource.java
@@ -0,0 +1,19 @@
+package com.tarcisiolopes.moneyapi.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tarcisiolopes.moneyapi.model.Categoria;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+	
+	@GetMapping
+	public ResponseEntity<Categoria> findAll(){
+		Categoria cat = new Categoria(1, "Receita");
+		return ResponseEntity.ok().body(cat);
+	}
+}
