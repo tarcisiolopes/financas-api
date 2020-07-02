274d6a2a9a1a732e803fd4b8a81c71202845276a
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-07-02 10:26:29 -0300

    Delete Recurso Pessoa

--- src/main/java/com/tarcisiolopes/moneyapi/resources/PessoaResource.java
+++ src/main/java/com/tarcisiolopes/moneyapi/resources/PessoaResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.tarcisiolopes.moneyapi.event.RecursoCriadoEvent;
 import com.tarcisiolopes.moneyapi.model.Pessoa;
@@ -42,5 +44,11 @@ public class PessoaResource {
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void apagar(@PathVariable Integer id) {
+		pessoaRepository.deleteById(id);
 	}
 }
